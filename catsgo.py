254f7b7c7c5f5b5babfc022e2e0c4779cd1bb63d
Denis Volk <denis.volk@gmail.com>
2021-02-15 19:53:00 +0000

    update to change in sp3 api

--- catsgo.py
+++ catsgo.py
@@ -91,7 +91,7 @@ def check_run(flow_name, run_uuid):
     url =  sp3_url + f'/flow/{ flow_name }/details/{ run_uuid }?api=v1'
     response =  session.get(url)
 
-    data = json.loads(response.text)
+    data = json.loads(response.text)['data']
     if not data['data']:
         return "Error"
         return
