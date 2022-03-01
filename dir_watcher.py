e7962a1d5cbef753b21a307fc0592b98af516293
Jez Swann <Jeremy.Swann@ndm.ox.ac.uk>
2022-03-01 08:56:00 +0000

    Fixed using two params with the same firtst letter in dir_watcher.

--- dir_watcher.py
+++ dir_watcher.py
@@ -369,14 +369,14 @@ def watch(
     watch_dir="/data/inputs/s3/oracle-test",
     bucket_name="catsup-test",
     max_submission_attempts=3,
-    workflow="ncov2019-artic-nf"
+    flow="ncov2019-artic-nf"
 ):
     """
     watch watch_dir for new directories that have the upload_done.txt file (signaling that an upload was successful)
 
     watch_dir example: /data/inputs/s3/oracle-test (for the catsup-test bucket. In the future we should probably name the directories the same as the bucket name!
     bucket_name: the bucket name that's mounted in the watch_dir directory (used by the pipeline to fetch the sample files)
-    workflow: currently a choice between ncov2019-artic-nf and sars-cov2_workflows
+    flow: currently a choice between ncov2019-artic-nf and sars-cov2_workflows
     """
     print(doc)
     watch_dir = Path(watch_dir)
@@ -400,7 +400,7 @@ def watch(
             apex_token = db.get_apex_token()
         for new_dir in new_dirs:  #  new_dir is the catsup upload uuid
             r = process_dir(
-                new_dir, watch_dir, bucket_name, apex_token, max_submission_attempts, workflow
+                new_dir, watch_dir, bucket_name, apex_token, max_submission_attempts, flow
             )
             if r:
                 # if we've started a run then stop processing and go to sleep. This prevents
